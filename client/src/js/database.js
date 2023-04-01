87a679b53a1f6deca1ed044fec0108378708be98
DylanF <dylanbrad10@gmail.com>
2023-03-31 20:40:41 -0400

    Finished application

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+// Takes in a content parameter and updates the text editor in the database.
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  return result;
+};
+// No parameter, just loading text editor
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  return result;
+};
 
 initdb();
